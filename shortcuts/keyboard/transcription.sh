ecc6903ebeb7a6d004f244bcdbf7017078b2b728
mbenaissa <mahmoud.benaissa@veepee.com>
2025-11-07 14:18:49 +0100

    feat: add auto-press Enter after transcription shortcut

--- shortcuts/keyboard/transcription.sh
+++ shortcuts/keyboard/transcription.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # Trigger keyboard shortcut Ctrl+Shift+W using cliclick
 # Commonly used for transcription apps
+# Then press Enter to start transcription immediately
 /opt/homebrew/bin/cliclick -w 50 kd:ctrl,shift t:w ku:ctrl,shift
+sleep 1
+osascript -e 'tell application "System Events" to keystroke return'
